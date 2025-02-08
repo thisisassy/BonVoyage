40fc6da426bc477b2dacfcafc328df7bfdc44961
Haruka Ashiuchi <chopperman1224@gmail.com>
2025-02-08 17:31:25 +0900

    Newtから記事を取ってくる

---
+++ src/lib/newt.ts
@@ -0,0 +1,14 @@
+import { createClient } from "newt-client-js";
+
+export interface Article {
+  title: string;
+  slug: string;
+  date: string;
+  body: string;
+}
+
+export const newtClient = createClient({
+  spaceUid: import.meta.env.NEWT_SPACE_UID,
+  token: import.meta.env.NEWT_CDN_API_TOKEN,
+  apiType: "cdn",
+});
